0bbe6ab36d9ab6c2584f991357fa74d483a642b9
Macarena Batisttella <macarena.batisttella@hotmail.com>
2022-07-05 16:51:27 -0300

    repo1

---
+++ js/index.js
@@ -0,0 +1,5 @@
+let producto = prompt("Que producto buscabas?");
+let cantidad = parseInt(prompt("Cuantas unidades?")); 
+let resultado = 590 * cantidad;
+alert("El costo total es de $" + resultado);
+
---
+++ js/turnos.js
@@ -0,0 +1,5 @@
+for (let i = 1; i < 20; i++){
+    let ingresarNombre = prompt("Ingresa tu nombre");
+    alert(" Turno N° "+i+" Nombre: "+ingresarNombre);
+    break
+}
